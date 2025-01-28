d98fb5ffb905c15bf7999edbd21eeaeae409d16e
Corben-Smith <corbensmiht782@gmail.com>
2025-01-28 16:00:09 -0500

    menu

--- main.py
+++ main.py
@@ -7,3 +7,38 @@ print("Hai :3")
 #Identify Major Event Intersections
 
 #Create Timeline
+
+
+
+
+#Main
+
+
+print("Choose which operation you want to do?")
+print("")
+print("1) Track Character Apperances")
+print("")
+print("2) Track Character Relationships")
+print("")
+print("3) Track Major Events")
+print("")
+print("4) Track Comic Release Timeline")
+
+ans = input()
+
+if ans == "1":
+    track_character_apperances()
+
+elif ans == "2":
+    track_character_relationships()
+
+elif ans == "3":
+    track_major_events()
+
+elif ans == "4":
+    create_timeline()
+
+else:
+    print("Invalid Input. Try again loser")
+
+print("Bai :333")
